f8b1b92f751c22aa965a4ec61a865944a268ac47
Lokesh-6X <s.lokeshkumar068@gmail.com>
2025-09-17 21:04:03 +0530

    Got an overvuew about if-else statemets in JS

---
+++ basics/conditionalStatements.js
@@ -0,0 +1,28 @@
+// if(condition){
+//     console.log("if is working");
+// }else{
+//     console.log("else is working");
+// }
+
+const isRaining = true;
+if(isRaining){
+    console.log("Take the umbrella");
+}else{
+    console.log("Enjoy the sunshine");
+}
+
+const finishedHomework = true;
+if(finishedHomework){
+    console.log("Enjoy");
+}else{
+    console.log("Do the home work");
+}
+
+const isCookiesLeft = false;
+if(isCookiesLeft){
+    console.log("Take some cookies");
+}else{
+    console.log("Time to bake some cookies");
+}
+
+// Logical and, or, not 
