d0af8471b0c73ff3df9c2689e224629d0e82d478
Lokesh-6X <s.lokeshkumar068@gmail.com>
2025-09-17 19:57:20 +0530

    Overview about datatypes in JS

--- basics/basics.js
+++ basics/basics.js
@@ -4,6 +4,8 @@
 //                 adding functionality to a website and allows for user interaction with the
 //                 website's content.
 
+// Keywords =  Reserevd wordsin Java Script
+
 // Vaiables = Storing data ex: let a = 10;
 //             Declaration using var, let, const
 //             let = local scope{}
@@ -16,3 +18,16 @@ const tax = 2.34;
 
 console.log(`${product} price: ${price+tax} (including tax)`);
 
+// Datatypes =     typeOf() {to get the type of value passed}
+//                 Types 1. Primitive, 2. Non-Primitive
+//                 Primitive: number, string, bool, null, undefined
+//                 Non-Primitive: Arrays, Objects 
+
+var a = null;
+var b;
+
+console.log(typeof(1));
+console.log(typeof("hello"));
+console.log(typeof(true));
+console.log(typeof(a));
+console.log(typeof(b));
