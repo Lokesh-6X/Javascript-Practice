e74dee8bcb39ddda0727397c5d27572c46036962
Lokesh-6X <s.lokeshkumar068@gmail.com>
2025-09-17 19:23:38 +0530

    Learned about differnt types of variables

---
+++ basics/basics.js
@@ -0,0 +1,18 @@
+//Basic Javascript Practice
+
+// JavaScript =    It is used to create interactive and dynamic web pages. It is responsible for
+//                 adding functionality to a website and allows for user interaction with the
+//                 website's content.
+
+// Vaiables = Storing data ex: let a = 10;
+//             Declaration using var, let, const
+//             let = local scope{}
+//             var = global scope{}
+//             const = constant value(value cannot be changed) & global scope
+
+const price = 100;
+const product = "Book";
+const tax = 2.34;
+
+console.log(`${product} price: ${price+tax} (including tax)`);
+
