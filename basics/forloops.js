91669ccb83304a3afd37dcf1fc432110272b152f
Lokesh-6X <s.lokeshkumar068@gmail.com>
2025-09-17 21:15:21 +0530

    Gone through for loops in JS

---
+++ basics/forloops.js
@@ -0,0 +1,22 @@
+// forloops =  It is a control flow statement used to repeadly execute a block of code based
+//             on a specific condition.
+
+// Print numbers from 1 to 10 using for loop
+// for(i=1;i<=10;i++){
+//     console.log(i);
+// }
+
+// Print all numbers in for loop from 1 to 10 with increment 2
+// for(i=1;i<=10;i+=2){
+//     console.log(i);
+// }
+
+// Print numbers 1 to 10 in reverse order
+// for(i=10;i>=1;i--){
+//     console.log(i);
+// }
+
+// Print 2 tables
+for(i=1;i<=10;i++){
+    console.log(i+"x"+2+"="+i*2);
+}
