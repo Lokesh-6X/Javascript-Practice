bbc5a9fa9b20021ec025eb42317f59aadcebd266
Lokesh-6X <s.lokeshkumar068@gmail.com>
2025-09-22 12:44:14 +0530

    Finished Tasks - Arrow Function

---
+++ tasks/task 3/evenNumbers.js
@@ -0,0 +1,3 @@
+const num = [1,2,3,4,5,6,7,8,9,11,22,33,44,55,66,10];
+const even = num.filter(n => n % 2 === 0);
+console.log(even);
---
+++ tasks/task 3/stringsLengths.js
@@ -0,0 +1,3 @@
+const words = ["Python", "Java", "Developer", "Programmer"];
+const lengths = words.map(word => word.length);
+console.log(lengths);
